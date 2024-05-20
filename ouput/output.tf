b6b6fa6da2d9d1cad37b9ec630960dbf948e4d43
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-05-20 19:10:58 +0530

    Auto push

--- ouput/output.tf
+++ ouput/output.tf
@@ -1,3 +1,12 @@
 output "demo" {
   value = "Welcome to terraform and we are going to learn basics of terraform "
 }
+
+output "demo1" {
+    value = 100
+  
+}
+output "demo2" {
+    value = "we are learning from basic to advance"
+  
+}
