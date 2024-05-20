df8e599bd5b7ca3bbaa67a155101d3f2f9552915
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-05-20 19:51:09 +0530

    Auto push

--- variables/variab.tf
+++ variables/variab.tf
@@ -5,3 +5,61 @@ variable "demo" {
 output "demos" {
   value = var.demo
 }
+
+variable "string" {
+  default = "Hellow world"
+}
+
+output "string" {
+  value = var.string
+}
+
+variable "number" {
+  default = 450
+}
+
+output "number" {
+  value = var.number
+}
+
+variable "boolean" {
+  default = true
+}
+
+output "boolean" {
+  value = var.boolean
+}
+
+variable "default" {
+  default = "Hellow guru"
+}
+
+output "default" {
+  value = var.default
+}
+
+variable "list" {
+  default = [
+    "hello" ,
+    255,
+    true,
+    "welcome to terraform"
+  ]
+}
+
+output "list" {
+  value = "${var.list[0]} and ${var.list[1]}"
+}
+
+variable "map_list" {
+  default = {
+    dev = "hello" 
+   number = 255
+   boolean = true
+    qa   = "welcome to terraform"
+  }
+}
+
+output "map" {
+  value = "${var.map_list[dev]} and ${var.map_list[qa]} is become ${var.map_list[boolean]}"
+}
